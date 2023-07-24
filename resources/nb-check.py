8e4faf0511b8fd3dda691ed0d9840f26af4db145
Kevin Smith <Kevin.Smith@octobergray.com>
2023-07-24 10:31:44 -0500

    Remove trailing empty cells

--- resources/nb-check.py
+++ resources/nb-check.py
@@ -15,14 +15,22 @@ for f in sys.argv[1:]:
         if k.startswith('singlestore'):
             del metadata[k]
 
+    cells = nb.get('cells', [])
+
     # Clear out execution metadata and cell output
-    for cell in nb.get('cells', []):
+    for cell in cells:
         if 'metadata' in cell:
             cell['metadata'] = {}
         if 'outputs' in cell:
             cell['outputs'] = []
         if 'execution_count' in cell:
             cell['execution_count'] = None
+
+    # Remove empty cells at the end of the notebook
+    end = len(cells) - 1
+    while end > 0 and 'source' in cells[end] and not cells[end]['source']:
+        cells.pop(end)
+        end -= 1
 
     with open(f, 'w') as outfile:
         outfile.write(json.dumps(nb, indent=2))
