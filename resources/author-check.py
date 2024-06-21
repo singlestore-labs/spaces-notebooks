f3867fdf1fc5c1ad449d46a13dc3f15489ee2de6
Francisco Silva <fsilva@singlestore.com>
2024-06-21 12:24:40 +0100

    allow image url, rename logo to image

--- resources/author-check.py
+++ resources/author-check.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
+import re
 import sys
 import tomllib
-
 
 def error(msg):
     print('ERROR:', msg, file=sys.stderr)
@@ -24,13 +24,17 @@ def check_author(author_path):
         if 'external' not in meta:
             error(f'No `external` in `meta` section of {author_path}')
 
-        # Logo is optional, but if defined a corresponding image must exist
-        if 'logo' in meta:
-            logo_id = meta['logo']
-            logo_filename = f'{logo_id}.png'
-            logo_path = os.path.join('common/images/author-images', logo_filename)
-            if not os.path.isfile(logo_path):
-                error(f'Logo image does not exist at {logo_path} for {author_path}')
+        # Image is optional, but if defined a corresponding image must exist
+        # Image can either be a URL or a filename in common/images/author-images
+        if 'image' in meta:
+            img_reference = meta['image']
+            is_url = bool(re.match(r'^https?://', img_reference))
+
+            if (not is_url):
+                img_filename = f'{img_reference}.png'
+                img_path = os.path.join('common/images/author-images', img_filename)
+                if not os.path.isfile(img_path):
+                    error(f'Author image does not exist at {img_path} for {author_path}')
 
 
 if __name__ == '__main__':
